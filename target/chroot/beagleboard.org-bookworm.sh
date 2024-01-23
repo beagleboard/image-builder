2c774d84dbaf2f4a63d92ba11c65f8a8704eac48
Seperis <seperis@gmail.com>
2024-01-23 04:28:47 -0600

    Update beagleboard.org-bookworm.sh
    Uncommented bb.org-overlays repository

--- target/chroot/beagleboard.org-bookworm.sh
+++ target/chroot/beagleboard.org-bookworm.sh
@@ -176,9 +176,9 @@ install_git_repos () {
 	git_branch="v6.1.x-Beagle"
 	git_clone_branch
 
-	#git_repo="https://github.com/beagleboard/bb.org-overlays"
-	#git_target_dir="/opt/source/bb.org-overlays"
-	#git_clone
+	git_repo="https://github.com/beagleboard/bb.org-overlays"
+	git_target_dir="/opt/source/bb.org-overlays"
+	git_clone
 
 	git_repo="https://github.com/mvduin/bbb-pin-utils"
 	git_target_dir="/opt/source/bbb-pin-utils"
